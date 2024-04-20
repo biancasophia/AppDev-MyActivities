6274f52b4b924698225a871e1f754d69a24f1b9b
biancamangundayao <biancasophiamangundayao@gmail.com>
2024-04-20 19:30:07 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pwa-Activity8',
+  webDir: 'www'
+};
+
+export default config;
